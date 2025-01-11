f13a57796cc9fcc7e8d9522da82c254e670759cc
Nikhil Upadhaya <nikhil.upadhaya001@gmail.com>
2025-01-10 20:15:18 -0500

    data base fixing

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -4,8 +4,8 @@
 import { sql } from "drizzle-orm";
 import {
   index,
-  integer,
   pgTableCreator,
+  serial,
   timestamp,
   varchar,
 } from "drizzle-orm/pg-core";
@@ -21,15 +21,16 @@ export const createTable = pgTableCreator((name) => `t3gallery_${name}`);
 export const images = createTable(
   "image",
   {
-    id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
+    id: serial("id").primaryKey(),
     name: varchar("name", { length: 256 }).notNull(),
-    url: varchar("name", { length: 1024 }).notNull(),
-    createdAt: timestamp("created_at", { withTimezone: true })
+    url: varchar("url", { length: 1024 }).notNull(),
+
+    userId: varchar("userId", { length: 256 }).notNull(),
+
+    createdAt: timestamp("created_at")
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
-    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date(),
-    ),
+    updatedAt: timestamp("updatedAt"),
   },
   (example) => ({
     nameIndex: index("name_idx").on(example.name),
